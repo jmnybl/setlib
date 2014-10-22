a92b7dc714ca162c12c6f53290776664f3b3fc3d
Filip Ginter <ginter@cs.utu.fi>
2014-10-22 17:49:49 +0300

    Test array in a DB

--- test_sql3.py
+++ test_sql3.py
@@ -1,10 +1,26 @@
 import pytset
 import sqlite3
 import db_index
+import struct
+
+def serialize_as_tset_array(tree_len,sets):
+    """
+    tree_len -> length of the tree to be serialized
+    sets: array of tree_len sets, each set holding the indices of the elements
+    """
+    indices=[]
+    for set_idx,s in enumerate(sets):
+        for item in s:
+            indices.append(struct.pack("@HH",set_idx,item))
+    res=struct.pack("@H",tree_len)+("".join(indices))
+    return res
 
 def make_dummy_db(conn):
     conn.execute("DROP TABLE IF EXISTS sets;")
     conn.execute("CREATE TABLE sets (name STRING PRIMARY KEY, set_data BLOB);")
+    conn.execute("DROP TABLE IF EXISTS setarrays;")
+    conn.execute("CREATE TABLE setarrays (name STRING PRIMARY KEY, setarray_data BLOB);")
+
 
 conn = sqlite3.connect('delme.db')
 make_dummy_db(conn)
@@ -17,6 +33,13 @@ conn.execute(u"INSERT OR IGNORE INTO sets VALUES (?,?)",(u"s2",buffer(s_ser)))
 s=pytset.PyTSet(129,range(52,110))
 s_ser=s.tobytes()
 conn.execute(u"INSERT OR IGNORE INTO sets VALUES (?,?)",(u"s3",buffer(s_ser)))
+conn.commit()
+
+sets=[set() for _ in range(20)]
+sets[0].add(5)
+sets[0].add(15)
+sets[1].add(17)
+conn.execute(u"INSERT OR IGNORE INTO setarrays VALUES (?,?)",(u"sa1",buffer(serialize_as_tset_array(20,sets))))
 conn.commit()
 conn.close()
 
