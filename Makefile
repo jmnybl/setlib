ecc83f22cb50ca678619905a7bf54bf450843db2
Filip Ginter <ginter@cs.utu.fi>
2014-11-18 18:07:01 +0200

    Removing functions unrelated to tset

--- Makefile
+++ Makefile
@@ -1,20 +1,14 @@
-TARGETS=pytset.so example_queries.so db_util.so
+TARGETS=pytset.so
 
 all: $(TARGETS)
 
 
-db_util.so: db_util.pyx db_util.pxd
-	python setup.py build_ext --inplace
-	mv setlib/db_util.so .
-
-pytset.so: tset.cpp tset.h pytset.pyx query_functions.cpp query_functions.h
+pytset.so: tset.cpp tset.h pytset.pyx pytset.pxd
 	python setup.py build_ext --inplace
 	mv setlib/pytset.so .
 
-example_queries.so: example_queries.pyx
-	python setup.py build_ext --inplace
-	mv setlib/example_queries.so .
 clean:
-	rm -rf $(TARGETS)
+	rm -rf $(TARGETS) setlib build
+
 
 
--- setup.py
+++ setup.py
@@ -3,21 +3,7 @@ from Cython.Build import cythonize
 
 setup(ext_modules = cythonize(
            "pytset.pyx",                 # our Cython source
-           sources=["tset.cpp","query_functions.cpp"],  # additional source file(s)
+           sources=["tset.cpp"],  # additional source file(s)
            language="c++",             # generate C++ code
       ))
-
-setup(ext_modules = cythonize(
-           "example_queries.pyx",                 # our Cython source
-           sources=["tset.cpp","query_functions.cpp"],  # additional source file(s)
-           language="c++",             # generate C++ code
-      ))
-
-setup(ext_modules = cythonize(
-           "db_util.pyx",                 # our Cython source
-           language="c++",             # generate C++ code
-           sources=["tset.cpp"],
-           libraries=["sqlite3"],
-      ))
-
 
